ee3dd5ce730ec318f5bcf885c0f8241c2ed1d92a
Akash581999 <akash581999@gmail.com>
2024-07-21 10:00:13 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,59 +1,15 @@
-// import { useState } from "react";
+import React from "react";
 import 'firebase/compat/auth';
 import "react-phone-input-2/lib/style.css";
 import Register from './Pages/Register';
-// import Login from './Pages/Login';
-// import ResetForm from './Pages/ResetForm';
-// import ResetPassword from './Pages/ResetPassword';
-// import Button from "react-bootstrap/Button";
-// import Alertify from './components/Alertify';
-// import Toastify from './components/Toastify';
 
 function App() {
-  // const [showAlert, setShowAlert] = useState(false);
-  // const [showToast, setShowToast] = useState(false);
-
-  // const handleShowAlert = () => {
-  //   setShowAlert(true);
-  // };
-  // const handleShowToast = () => {
-  //   setShowToast(true);
-  // };
 
   return (
     <>
       <div className='container w-100'>
         <h1 className="fs-1 text-dark">Phone Verification</h1>
         <Register />
-        {/* <br />
-        <Login />
-        <br />
-        <ResetPassword />
-        <br />
-        <ResetForm /> */}
-
-        {/* <Button onClick={handleShowAlert} className="mb-3 mx-3">
-          Show Alert
-        </Button>
-        <Alertify
-          show={showAlert}
-          setShowAlert={setShowAlert}
-          color={"success"}
-          topic="Login Successfully!"
-          message="Welcome, Akash Kumar!"
-          buttonText="OK"
-        /> */}
-
-        {/*<Button onClick={handleShowToast} className="mb-3 mx-3">
-          Show Toast
-        </Button>
-        <Toastify
-          show={showToast}
-          setShowToast={setShowToast}
-          color={"info"}
-          topic="Login Successfully!"
-          message="Welcome, Akash Kumar!"
-        /> */}
       </div>
     </>
   );
